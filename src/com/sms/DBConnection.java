36dd97697c5ed2635ffcc66534e10c9553ab51af
Chetas35 <chetasj512@gmail.com>
2026-02-14 16:23:12 +0530

    Remove hardcoded DB password

--- src/com/sms/DBConnection.java
+++ src/com/sms/DBConnection.java
@@ -7,7 +7,7 @@ public class DBConnection {
 	
 	private static final String URL = "jdbc:mysql://localhost:3306/student_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "chetasj@05";
+    private static final String PASSWORD = "your_password_here";
 
     public static Connection getConnection() {
         try {
